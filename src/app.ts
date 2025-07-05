bea8c8cc331abd99958e11f0b70d28163d3ae094
Shaid Islam <shaid85@gmail.com>
2025-07-05 16:39:10 +0600

    cors add

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
  * app.ts - route handle, middleware, route related error
  */
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 import { bookRoutes } from './app/controllers/book.controller'
 import { errorHandler } from './middlewares/errorHandler'
 import { borrowRoutes } from './app/controllers/borrow.controller'
@@ -9,7 +10,11 @@ import { borrowRoutes } from './app/controllers/borrow.controller'
 const app: Application = express()
 // Middleware to parse JSON requests
 app.use(express.json())
-
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://assingnment-4-chi.vercel.app/'],
+  })
+)
 app.use('/api/books', bookRoutes)
 app.use('/api/borrow', borrowRoutes)
 
